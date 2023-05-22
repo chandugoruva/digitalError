f2a99de1c4fa60a5dd3e0773991f516566b9acb6
chandu <chandugoruva@gmail.com>
2023-05-22 08:58:18 +0000

    digitalError

---
+++ src/components/DigitalTimer/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+import {Component} from 'react'
+
+class DigitalTimer extends Component {
+  render() {
+    return (
+      <div className="bg-color">
+        <div className="bg-timer1">
+          <div className="timer">
+            <h1>20</h1>
+          </div>
+          <p>hiii</p>
+        </div>
+      </div>
+    )
+  }
+}
+export default DigitalTimer
